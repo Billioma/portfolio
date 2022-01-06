4e7211d4fe9bea54af2c0abfe8d427499f6352a1
Omar <90000500+Billioma@users.noreply.github.com>
2022-01-06 15:41:47 +0100

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = () => (
         My Personal Portfolio
       </SectionTitle>
       <SectionText>
-        Enthusiastic web dev with 1 year of Front-End experience
+        Enthusiastic web dev with 2 years of Front-End experience
       </SectionText>
     </LeftSection>
   </section>
